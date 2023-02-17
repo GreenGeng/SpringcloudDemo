59588b5deffd7bbbc1f0f950414f248cd042cc00
gengyuan <2088034881@qq.com>
2023-02-17 20:45:50 +0800

    first commit

---
+++ src/main/java/com/learnbyheart/springcloudtest1/SpringCloudTest1Application.java
@@ -0,0 +1,13 @@
+package com.learnbyheart.springcloudtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/learnbyheart/springcloudtest1/SpringCloudTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnbyheart.springcloudtest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
